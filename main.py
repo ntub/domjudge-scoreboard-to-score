25db93d4bc564d55711137347dcdba7b81be0435
Arthur Chang <arthurc0102@gmail.com>
2024-12-04 12:27:36 +0800

    feat: get site_id in team name not from password file

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import csv
 import os
 import sys
-import copy
 
 from typing import Dict, List
 
@@ -19,15 +18,9 @@ def load_data(filename: str) -> List[Dict[str, str]]:
 
 def main() -> None:
     assert len(sys.argv) >= 2, "No file set."
-    assert len(sys.argv) >= 3, "No password file set."
 
     filename = sys.argv[1]
-    password_filename = sys.argv[2]
     data = load_data(filename)
-
-    user_mapping = {}
-    for user_info in load_data(password_filename):
-        user_mapping[f'{user_info["學校名稱"]}_{user_info["選手姓名"]}'] = user_info["崗位編號"]
 
     assert data, "No data found in file."
 
@@ -49,12 +42,14 @@ def main() -> None:
     for datum in data:
         rank_str = datum["Rank"]  # 排名
         team_affiliation = datum["TeamAffiliation"]  # 校名
-        team_name = datum["TeamName"]  # 名字
+        team_name = datum["TeamName"]  # 隊名（崗位編號_名字）
         solved_count_str = datum["SolvedCount"]  # 解題數
         total_time = datum["Score"]  # 術科時間
 
         if not rank_str and not team_affiliation and not team_name:
             continue  # 跳過加總
+
+        site_id, name = team_name.split("_", 1)
 
         if not rank_str:
             assert last_rank is not None, "Can't found rank."
@@ -72,12 +67,11 @@ def main() -> None:
         rank_point = 100 - int(rank) + 1  # 排名點數
         solved_point = 100 - (max_solved_count - solved_count) * 5  # 解題點數，跟最多比每多一台扣五分
 
-        site_id = user_mapping[f"{team_affiliation}_{team_name}"]
         row = [
             rank,
             team_affiliation,
             site_id,
-            team_name,
+            name,
             total_time,
             solved_count,
             rank_point,
